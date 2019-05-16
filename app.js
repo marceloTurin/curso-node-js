aca533bca0d4290d07fef4fb64c89367d739f5b5
Marcelo Sousa <marcelo.sousa@lbca.com.br>
2019-05-16 20:02:56 -0300

    Aula 10 - Exibindo HTML

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-var http = require('http'); // Protocoto http
+const express = require('express'); // Importando modulo do express
+const app = express(); //Criando uma instancia do express na variavel app
 
 
+//Rota principal do app
+app.get("/",(req,res)=>{ //req é uma requisição recebida, e rep são as respostas da requisição
+    res.sendFile(`${__dirname}/html/index.html`); //
+});
 
-http.createServer(function(req,res){ //Cria um servidor na porta 8081
+//Rota sobre
+app.get("/sobre",(req,res)=>{
+    res.sendFile(`${__dirname}/html/sobre.html`);
+    //res.send("Minha Pagina Sobre"); // Envia uma mensagem do usuário
+});
 
-    res.end("Hello World"); //Função que envia uma mensagem
-}).listen(8081);
+//Rota do Blog
+app.get("/blog",(req,res)=>{
+    res.send("Bem Vindo ao Meu Blog!");
+});
 
-console.log("Servidor esta rodando!")
+app.get('/ola/:nome/:cargo/:cor',(req,res)=>{ //Passando parametros na rota
+    res.send(`<h1>Olaaa ${req.params.nome}<h1> 
+              <h2>Seu cargo e: ${req.params.cargo}</h2> 
+              <h3>Sua cor favorita e: ${req.params.cor}</h3>`
+            ); //Obtendo os parametros da requisição
+  
+})
+
+
+//Abre um servidor na porta 8081
+app.listen(8081,()=>{ // Função callback que é chamado quando o evento é disparado
+  console.log("Servidor Rodando na url http//localhost:8081")  
+}); 
