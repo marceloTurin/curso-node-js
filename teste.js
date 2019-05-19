25d0de82e8aaa37871f300ec65f3f2b8db2a1043
marcelo sousa <marcelo.sousa1997@hotmail.com>
2019-05-18 22:58:54 -0300

    Aula 18 - models no sequelize

--- teste.js
+++ teste.js
@@ -11,4 +11,50 @@ sequelize.authenticate()//Verifica se houve conexão com o banco
 })
 .catch((erro)=>{
     console.log(`Falha ao se conectar: ${erro}`);
-}); 
+})
+
+
+//CREATE TABLE no Sequelize
+const Postagem = sequelize.define('postagens',{
+    titulo:{
+        type: Sequelize.STRING //tem limite de caracteres
+    },
+    conteudo : {
+        type: Sequelize.TEXT // nao tem limite de texto
+    }
+
+})
+
+Postagem.sync({force: true}) //Execucao do create table
+
+//INSERT INTO no sequelize
+Postagem.create({
+    titulo: "Um Titulo Qualquer",
+    conteudo: "Um conteudo qualuer"
+})
+
+const Usuarios  = sequelize.define('usuarios',{
+    nome:{
+        type: Sequelize.STRING
+    },
+    sobrenome:{
+        type: Sequelize.STRING
+    },
+    idade:{
+        type: Sequelize.INTEGER
+    },
+    email:{
+        type: Sequelize.STRING
+    }
+})
+
+Usuarios.create({
+    nome :"Marcelo",
+    sobrenome : "Sousa",
+    idade : "21",
+    email: "marcelo.sousa@email.com"
+})
+
+Usuarios.sync({force:true});
+
+
