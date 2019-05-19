31648c83bdc2640e4990769ea1e0693bcd89a286
marcelo sousa <marcelo.sousa1997@hotmail.com>
2019-05-19 17:16:45 -0300

    Aula 24 - Pegando dados do formulario

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express'); // Importando modulo do express
 const app = express(); //Criando uma instancia do express na variavel app
 const handlebars = require('express-handlebars');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const  Post = require('./models/Post');
 
 
 //Config
@@ -13,14 +14,28 @@ const bodyParser = require('body-parser')
     app.use(bodyParser.json());
 
 //Rotas
+
+    app.get('/',(req,res)=>{
+        res.render('home');
+    })
+
     app.get('/cad',(req,res)=>{
        res.render('formulario'); //Renderiza o html da pasta view
     })
 
+
+    //Rota que adiciona postagens no banco
     app.post('/add',(req,res)=>{
-        req.body.conteudo //Pega o conteudo do formulario atraves do name
-        res.send(`Texto: ${req.body.titulo} Conteudo ${req.body.conteudo}`)
-        res.send("Formulario recebido");
+       Post.create({  //função que da insert no banco
+           titulo : req.body.titulo, //metodo que pega informação no formulario
+           conteudo: req.body.conteudo
+       })
+       .then(()=>{ //função callback caso o insert de certo
+           res.redirect('/'); //função que redireciona para alguma rota
+       })
+       .catch((erro)=>{ // função callback caso de um erro
+            res.send(`Houve um erro : ${erro}`)
+       })
     })
 
 
